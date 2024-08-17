3865e18689b26120ce11304a4343209fed0070e4
viktoriiaignateva <ignateva.victoriia@gmail.com>
2024-08-17 21:54:39 +0200

    continue shopping list component create

--- app/src/components/SoppingLists.js
+++ app/src/components/SoppingLists.js
@@ -1,18 +1,24 @@
-function Item() {
-    return (
-        <li> 1 </li>
-    )
-}
+const soppingLists = [
+    'grocery',
+    'cloths',
+    'cats'
+]
+
+// function Item() {
+//     return (
+//         <li>{listItem}</li>
+//     )
+// }
 
 export default function SoppingLists() {
+    const listItem = soppingLists.map(list => <li>{list}</li>)
+
     return (
         <div className="flex justify-center bg-gray-100">
             <div className="w-full max-w-md p-4 bg-white">
                 <h1 className="text-2xl font-semibold text-center mb-4">Your Lists</h1>
                 <div className="grid grid-cols-1 gap-4">
-                    <Item/>
-                    <Item/>
-                    <Item/>
+                    <ul>{listItem}</ul>
                 </div>
             </div>
         </div>
